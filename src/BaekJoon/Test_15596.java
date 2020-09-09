6115f41be17301540255e8affd5e8a2b80fd12be
minseominseo <chj2171@gmail.com>
2020-09-09 16:12:01 +0900

    함수 문제하나와 별찍기 하나 백준 알고리즘 문제

---
+++ src/BaekJoon/Test_15596.java
@@ -0,0 +1,9 @@
+package BaekJoon;
+public class Test_15596 {
+
+    public static long sum(int []a){
+        long re =0;
+        for(long i: a) re = re +i;
+        return re;
+    }
+}
---
+++ src/BaekJoon/star9_2446.java
@@ -0,0 +1,31 @@
+package BaekJoon;
+
+import java.io.*;
+
+public class star9_2446 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        br.close();
+        star(num);
+
+    }
+    static void star(int num) throws IOException{
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for(int i=0; i<num; i++){
+            for(int t =0; t<i; t++)
+                bw.write(' ');
+            for(int j = (num-i)*2-1; j>0; j--)
+                bw.write('*');
+            bw.newLine();
+        }//위의 별
+        for(int i =num-2; i>-1; i--){
+            for(int t =0; t< i; t++)
+                bw.write(' ');
+            for(int j = 0; j<(num-i)*2-1; j++)
+                bw.write('*');
+            bw.newLine();
+        }
+        bw.close();
+    }
+}
