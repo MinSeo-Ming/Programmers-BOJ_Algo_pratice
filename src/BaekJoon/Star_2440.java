de7a20ffc516ba18c09e97d8c6b7e275305419d2
minseominseo <chj2171@gmail.com>
2020-09-15 23:53:16 +0900

    별찍기 2442 조금 변형해서 계산 완료

---
+++ src/BaekJoon/Star_2440.java
@@ -0,0 +1,15 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Star_2442 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for(int i=num-1; i>=0; i--){
+            for(int k =0; k<=i; k++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
