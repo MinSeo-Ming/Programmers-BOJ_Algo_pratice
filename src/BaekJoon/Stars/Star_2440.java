0c6ca6197a2ac33f23c04f128044bb4e88d475b0
minseominseo <chj2171@gmail.com>
2020-09-23 00:05:16 +0900

    별찍기 모음집!

--- src/BaekJoon/Stars/Star_2440.java
+++ src/BaekJoon/Stars/Star_2440.java
@@ -2,14 +2,12 @@ package BaekJoon;
 
 import java.util.Scanner;
 
-public class Star_2442 {
+public class Star_2440 {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         for(int i=num-1; i>=0; i--){
-            for(int k =0; k<i; k++)
-                System.out.print(" ");
-            for(int j = 0; j<2*(num-i)-1; j++)
+            for(int k =0; k<=i; k++)
                 System.out.print("*");
             System.out.println();
         }
--- src/BaekJoon/Stars/Star_2442.java
+++ src/BaekJoon/Stars/Star_2442.java
@@ -7,7 +7,10 @@ public class Star_2442 {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         for(int i=num-1; i>=0; i--){
-            for(int k =0; k<=i; k++)
+            for(int k =0; k<i; k++) {
+                System.out.print(" ");
+            }
+            for(int j = 0; j<2*(num-i)-1; j++)
                 System.out.print("*");
             System.out.println();
         }
