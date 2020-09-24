7d97ce9a637b1f130939fa6478ba64decb35d515
minseominseo <chj2171@gmail.com>
2020-09-24 14:17:25 +0900

    별찍기 시리즈~

---
+++ src/BaekJoon/Stars/stars_10995.java
@@ -0,0 +1,34 @@
+package BaekJoon.Stars;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class stars_10995 {
+    public static void main(String args[])throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int x = Integer.parseInt(br.readLine());
+        br.close();
+        if(x == 1){
+            System.out.println("*");
+            return;
+        }
+        for(int i= 1; i<=x; i++) {
+            if(i%2==1)
+                star(x,0);
+            else
+                star(x,1);
+        }
+
+    }
+    static void star(int i,int re){
+        for (int j = 0; j < 2 * i  ; j++) {
+            if(j%2 ==re)
+                System.out.print("*");
+            else
+                System.out.print(" ");
+
+        }
+        System.out.println();
+    }
+}
