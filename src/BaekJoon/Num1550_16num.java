7959f50ffd231f525593c32d89275545ccaefcf1
minseominseo <chj2171@gmail.com>
2020-09-25 22:43:47 +0900

    16진수로 바꾸기

---
+++ src/BaekJoon/Num1550_16num.java
@@ -0,0 +1,13 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class Num1550_16num {
+    public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        System.out.println(Integer.parseInt(a,16));
+
+    }
+}
+}
