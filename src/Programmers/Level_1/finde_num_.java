320b9a8684121922e1bf31a542653e22c17822cb
MinSeo <42549035+MinSeo-Ming@users.noreply.github.com>
2020-12-12 23:07:34 +0900

    소수 갯수 세기

---
+++ src/Programmers/Level_1/finde_num_.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int[]list = new int[n+1];
+        
+        Arrays.fill(list, 1);
+        list[0]=0;
+        list[1]=0;
+   
+        
+        for(int i=2; (i*i)<=n; i++){
+            if(list[i]==1){
+                for(int j = i*i; j<=n;j+=i)
+                    list[j]=0;
+            }
+        }  
+        for(int c :list){
+            if(c==1)
+                answer++;
+        }
+
+        return answer;
+    }
+}
